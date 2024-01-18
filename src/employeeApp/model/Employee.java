1f29500cd93921bed93844f0cb026fd9ed81673c
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-18 22:59:42 +0300

    her degisken icin getter ve setter methodlari olusturuldu

--- src/employeeApp/model/Employee.java
+++ src/employeeApp/model/Employee.java
@@ -22,4 +22,44 @@ public class Employee {
         this.password = password;
         this.healthPlans = healthPlans;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String[] getHealthPlans() {
+        return healthPlans;
+    }
+
+    public void setHealthPlans(String[] healthPlans) {
+        this.healthPlans = healthPlans;
+    }
 }
