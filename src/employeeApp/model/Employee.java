d5290f607c6615cebca96612332fb5d282f03397
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-18 22:58:51 +0300

    Employee class'i icin 2 adet constructor olusturuldu

--- src/employeeApp/model/Employee.java
+++ src/employeeApp/model/Employee.java
@@ -6,4 +6,20 @@ public class Employee {
     private String email;
     private String password;
     private String[] healthPlans;
+
+    //NOT: 2 adet constructor olusturdugum icin burada constructor overloading
+    // yapmis olduk.
+
+    public Employee(int id, String fullName){
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public Employee(int id, String fullName, String email, String password, String[] healthPlans) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.healthPlans = healthPlans;
+    }
 }
