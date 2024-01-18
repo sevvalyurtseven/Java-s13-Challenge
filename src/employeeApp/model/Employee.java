60e1482bebbfadea79e1a5ad39ebdacd6f00e16f
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-18 22:54:22 +0300

    Employee class'i olusturuldu ve degiskenler tanimlandi

---
+++ src/employeeApp/model/Employee.java
@@ -0,0 +1,9 @@
+package employeeApp.model;
+
+public class Employee {
+    private int id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String[] healthPlans;
+}
