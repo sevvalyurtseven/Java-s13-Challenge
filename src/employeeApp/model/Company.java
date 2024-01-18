bafce392b2e8eaa524bb08cc1bea419819043a33
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-18 23:58:39 +0300

    POJO (boilerplate code) olusturuldu. (minimum gereklilikler)

--- src/employeeApp/model/Company.java
+++ src/employeeApp/model/Company.java
@@ -1,8 +1,59 @@
 package employeeApp.model;
 
+import java.util.Arrays;
+
 public class Company {
     private int id;
     private String name;
     private double giro;
     private String[] developerNames;
+
+    public Company(int id, String name, double giro, String[] developerNames) {
+        this.id = id;
+        this.name = name;
+        this.giro = giro;
+        this.developerNames = developerNames;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGiro() {
+        return giro;
+    }
+
+    public void setGiro(double giro) {
+        this.giro = giro;
+    }
+
+    public String[] getDeveloperNames() {
+        return developerNames;
+    }
+
+    public void setDeveloperNames(String[] developerNames) {
+        this.developerNames = developerNames;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", giro=" + giro +
+                ", developerNames=" + Arrays.toString(developerNames) +
+                '}';
+    }
 }
